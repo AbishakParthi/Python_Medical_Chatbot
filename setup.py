17a6a3256164329681459b842fa2cad5a226d882
AbishakParthi <abishakeparthi4@gmail.com>
2026-02-10 23:34:04 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name = "Python Medical Chatbot",
+    version = "0.0.0",
+    author = "Abishak Parthi",
+    author_email = "abishakeparthi4@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
